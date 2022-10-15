d590df9e90518e320bf80782d525f0d87628fead
Pedro Victor <pedro20002victor@gmail.com>
2022-10-14 22:19:03 -0300

    Create file routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import { PrismicProvider } from '@prismicio/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { createRoot } from 'react-dom/client'
 import { client } from './components/services/prismic'
 import theme from './styles/theme'
+
+const container = document.getElementById('root');
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ChakraProvider theme={theme}>
