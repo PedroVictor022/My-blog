d00e01ed9ea187120bb0736b898618494cdca108
Pedro Victor <pedro20002victor@gmail.com>
2022-10-15 11:11:23 -0300

    New types posts

--- src/helpers/prismic/home.ts
+++ src/helpers/prismic/home.ts
@@ -2,5 +2,5 @@ import { useSinglePrismicDocument } from "@prismicio/react"
 
 export const handleDocument = () => {
    const [document] = useSinglePrismicDocument('home');
-   console.log(document);
+   console.log(document)
 }
